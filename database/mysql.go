456b02902e58c4e8a2f703ec27074ddebd3986ae
Emanuel <emaa.forlin@gmail.com>
2024-07-24 12:16:12 -0300

    database migration: fix

--- database/mysql.go
+++ database/mysql.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/emaforlin/accounts-service/config"
+	"github.com/emaforlin/accounts-service/x/entities"
 	"github.com/hashicorp/go-hclog"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,6 +16,19 @@ type mysqlDatabase struct {
 	db *gorm.DB
 }
 
+// AutoMigrate implements Database.
+func (p *mysqlDatabase) AutoMigrate() error {
+	var errorStr string
+	err1 := p.GetDb().AutoMigrate(&entities.InsertPersonDto{})
+	err2 := p.GetDb().AutoMigrate(&entities.InsertFoodPlaceDto{})
+
+	errorStr = fmt.Sprintf("%v%v", err1, err2)
+	if len(errorStr) != 0 {
+		return errors.New("error migrating database")
+	}
+	return nil
+}
+
 func NewMySQLDatabase(cfg *config.Config, l hclog.Logger) Database {
 	dblogger := logger.New(l.Named("database").StandardLogger(&hclog.StandardLoggerOptions{}), logger.Config{
 		LogLevel: logger.Error,
@@ -22,7 +37,7 @@ func NewMySQLDatabase(cfg *config.Config, l hclog.Logger) Database {
 		DSN:                       fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.Db.User, cfg.Db.Passwd, cfg.Db.Host, cfg.Db.Name),
 		SkipInitializeWithVersion: true,
 	}), &gorm.Config{
-		Logger: dblogger,
+		Logger: dblogger.LogMode(logger.Silent),
 	})
 
 	if err != nil {
