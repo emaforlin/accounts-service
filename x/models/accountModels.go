ad80e0ffb86631f939a84f91833347d1596d525a
Emanuel <emaa.forlin@gmail.com>
2024-08-07 21:26:18 -0300

    feat(models): add method to facilitate rpc calls

--- x/models/accountModels.go
+++ x/models/accountModels.go
@@ -1,5 +1,7 @@
 package models
 
+import pb "github.com/emaforlin/accounts-service/x/handlers/grpc/protos"
+
 type (
 	GetUserId struct {
 		Username    string `validate:""`
@@ -38,3 +40,66 @@ type (
 		Role        string `validate:""`
 	}
 )
+
+func (m *GetUserId) ToRPCStruct() *pb.GetUserIdRequest {
+	if m.Email != "" {
+		return &pb.GetUserIdRequest{
+			Identifiers: &pb.GetUserIdRequest_Email{Email: m.Email},
+		}
+	} else if m.Username != "" {
+		return &pb.GetUserIdRequest{
+			Identifiers: &pb.GetUserIdRequest_Username{Username: m.Username},
+		}
+	}
+	return &pb.GetUserIdRequest{
+		Identifiers: &pb.GetUserIdRequest_PhoneNumber{PhoneNumber: m.PhoneNumber},
+	}
+}
+
+func (m *AddPersonAccountData) ToRPCStruct() *pb.AddPersonAccountRequest {
+	return &pb.AddPersonAccountRequest{
+		Username:    m.Username,
+		PhoneNumber: m.PhoneNumber,
+		Email:       m.Email,
+		Password:    m.Password,
+		FirstName:   m.FirstName,
+		LastName:    m.LastName,
+	}
+}
+
+func (m *GetAccountData) ToRPCStruct() *pb.GetAccountDetailsRequest {
+	return &pb.GetAccountDetailsRequest{Userid: m.Id}
+}
+
+func (m *AddFoodPlaceAccountData) ToRPCStruct() *pb.AddFoodPlaceAccountRequest {
+	return &pb.AddFoodPlaceAccountRequest{
+		Username:     m.Username,
+		PhoneNumber:  m.PhoneNumber,
+		Email:        m.Email,
+		Password:     m.Password,
+		BusinessName: m.BusinessName,
+		Location:     m.Location,
+		Tags:         m.Tags,
+	}
+}
+
+func (m *LoginAccount) ToRPCStruct() *pb.LoginUserRequest {
+	if m.Email != "" {
+		return &pb.LoginUserRequest{
+			Identifiers: &pb.LoginUserRequest_Email{Email: m.Email},
+			Password:    m.Password,
+			Role:        m.Role,
+		}
+	} else if m.Username != "" {
+		return &pb.LoginUserRequest{
+			Identifiers: &pb.LoginUserRequest_Username{Username: m.Username},
+			Password:    m.Password,
+			Role:        m.Role,
+		}
+	}
+	return &pb.LoginUserRequest{
+		Identifiers: &pb.LoginUserRequest_PhoneNumber{PhoneNumber: m.PhoneNumber},
+		Password:    m.Password,
+		Role:        m.Role,
+	}
+}
